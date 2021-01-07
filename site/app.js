cb60e0636f577166cef8a753aa0f08c1b7434677
leaBaglietto <leandro.baglietto@gmail.com>
2021-01-07 00:36:42 -0300

    el ferretero corre en local

--- site/app.js
+++ site/app.js
@@ -1,14 +1,15 @@
 const express = require ('express');
-const path = require ('path');
+const path = require ('path')
 
 const app = express ();
 
-const publicPath = path.join (__dirname, './public');
-app.use( express.static(publicPath));
+app.listen (3000, () => {
+    console.log ('ferretero atendiendo');
+});
 
 app.get ('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'))
+       res.sendFile(path.join(__dirname, './src/views/index.html'));
 })
-app.listen (3030, () => {
-    console.log ('Servidor LAFE - HERRAMIENTA Funciona')
-}); 
+
+const publicPath = path.join (__dirname, './public');
+app.use( express.static(publicPath));
