1e856a5d7b3c255f616fb7a27364c43fa90f1c46
leaBaglietto <leandro.baglietto@gmail.com>
2021-01-19 17:43:58 -0300

    rutas funcionando pero no renderiza

---
+++ site/src/app.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+const path = require ('path');
+
+
+const usersRoutes = require ('./routes/userRoutes')
+
+const app = express ();
+
+app.listen (3000, () => {
+    console.log ('-------- BIENVENIDOS A LAFE --------');
+    console.log ('ferretero atendiendo en la caja 3000');
+    console.log ('--- #quedateEnCasa compra onLine ---')
+});
+
+app.get ('/', (req, res) => {
+       res.sendFile(path.join(__dirname, './src/views/index.html'));
+})
+/*
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './src/views/register.html'));
+}); */ 
+
+app.use ('/user',usersRoutes)
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'));
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+const publicPath = path.join (__dirname, './public');
+app.use( express.static(publicPath));
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
