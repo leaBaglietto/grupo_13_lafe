b38cac6505027bd8cc0978bb72d6163504982975
leaBaglietto <leandro.baglietto@gmail.com>
2021-02-01 21:20:57 -0300

    actualizando el app.js

--- site/app.js
+++ site/app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const path = require ('path');
 
 const mainRoutes = require ('./src/routes/mainRoutes')
-// const usersRoutes = require ('./src/routes/mainRoutes')
+
 
 const app = express ();
 
@@ -12,35 +12,13 @@ app.listen (3000, () => {
     console.log ('--- #quedateEnCasa compra onLine ---')
 });
 
+// --- Configuracion ---
 // LLAMAMOS A EJS PARA QUE SEA NUESTRO TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 app.use ('/', mainRoutes);
 
 app.set ('views', 'src/views')
 
- /*
-app.get ('/', (req, res) => {
-       res.sendFile(path.join(__dirname, './views/index.html'));
-})
-/*
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './src/views/register.html'));
-}); */ 
-
-//Ruteador //
-
-/*
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productCart.html'));
-});
-
-app.get('/product', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'));
-}); */
 
 const publicPath = path.join (__dirname, './public');
 app.use( express.static(publicPath));
