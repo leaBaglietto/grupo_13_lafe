7194918557f7ba83d934b3f807e0340bd6405134
leaBaglietto <leandro.baglietto@gmail.com>
2021-03-08 21:35:25 -0300

    Agregando usuarios OK

--- site/src/controllers/usersController.js
+++ site/src/controllers/usersController.js
@@ -1,22 +1,25 @@
 const fs = require('fs');
 const path = require('path');
+const jsonTable = require('../data/jsonTable');
+const usersTable = jsonTable('users');
 
 let users = fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8');
 users = JSON.parse(users)
 
-//const jsonTable={
-//userCreate(row) {
-// let rows = this.readFile();
-// row.id = this.nextId();
-//rows.push(row);
-//this.writeFile(rows);
-//return row.id;}
+
 module.exports = {
     userCreate: (req, res) => {
         res.render('userCreate')
     },
     processCreate: (req, res) => {
-        res.send(req.body)
+        let newUser = req.body
+        delete newUser.repassword
+        // newUser.password = hashear contraseña //
+        // Cuando tengamos imagenes //
+        let userId = usersTable.create(newUser);
+        console.log (userId)
+        /* res.redirect('/') */
+        res.send('usuario creado' + userId)
     }
 
 };
