5cc7789e2d232f747d978b6785b8d74430a96119
leaBaglietto <leandro.baglietto@gmail.com>
2021-04-25 00:42:35 -0300

    Arreglos de HTML, CSS y agregamos error404

--- site/src/routes/productRouter.js
+++ site/src/routes/productRouter.js
@@ -3,9 +3,10 @@ const productController = require("../controllers/productController");
 const router=express.Router();
 
 const usersController= require("../controllers/productController");
-
+//llamamos al formulario de producto
 router.get('/create', productController.agregarProducto);
 
+//procesar el formulario de producto
 router.post('/create',productController.productoAgregado);
 
 
