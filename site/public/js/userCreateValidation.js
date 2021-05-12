95d5a7cc68fb81dd51835343549e816d6bf51631
leaBaglietto <leandro.baglietto@gmail.com>
2021-05-12 20:12:49 -0300

    Arreglos HTML, Validaciones, profile

--- site/public/js/userCreateValidation.js
+++ site/public/js/userCreateValidation.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", function () {
     let formulario = document.querySelector("form.reservation");
+    console.log ("estamos en validaciones")
 
     const form = document.getElementById('form');
     const first_name = document.getElementById('first_name');
@@ -8,48 +9,73 @@ window.addEventListener("load", function () {
     const password = document.getElementById('password');
     const password2 = document.getElementById('password2');
 
-    formulario.addEventListener("submit", function (e) {
-        e.preventDefault();
-        checkInputs();
+    let first_nameSuccess = false; /* hacer esto con todas las variables de arriba */
+    let last_nameSuccess = false;
+    let emailSuccess = false;
+    let passwordSuccess = false;
+    let password2Success = false;
+
+
+    form.addEventListener("submit", function (e) {
+        if(first_nameSuccess == true) { }
+        else { 
+            e.preventDefault();
+            checkInputs();
+        }
+ 
     });
 
     function checkInputs() { 
-        const first_nameValue = first_name.nodeValue.trim();
+        const first_nameValue = first_name.value.trim(); /* TRIM Elimina cualquier caracter vacio al principio o final del input */
         const last_nameValue = last_name.value.trim();
         const emailValue = email.value.trim();
         const passwordValue = password.value.trim();
         const password2Value = password2.value.trim();
 
+       
+
         if (first_nameValue === '') {
-            setErrorFor(first_name, 'Aca te pido tu nombre')
+            setErrorFor(first_name, 'Aca te pido tu nombre');
+            first_nameSuccess = false; /* hacer esto con todas las variables de arriba */
         } else {
+            first_nameSuccess = true; /* hacer esto con todas las variables de arriba */
             setSuccessFor(first_name)
         };
 
         if (last_nameValue === '') {
-            setErrorFor(last_name, 'Aca te pido tu Apellido')
+            setErrorFor(last_name, 'Aca te pido tu Apellido');
+            last_nameSuccess = false;
         } else {
+            last_nameSuccess = true;
             setSuccessFor(last_name);
         }
 
         if (emailValue === '') {
-            setErrorFor(email, 'Dale amigo dejate un mail')
+            setErrorFor(email, 'Dale amigo dejate un mail');
+            emailSuccess = false;
         } else if (!isEmail(emailValue)) {
+            emailSuccess = false;
             setErrorFor(email, 'Dale amigo esto no es un mail valido!!! ');
         } else {
+            emailSuccess = true;
             setSuccessFor(email);
         }
         if (passwordValue === '') {
-            setErrorFor(password, 'Metele una contraseña')
+            passwordSuccess = false;
+            setErrorFor(password, 'Tu contraseña debe tener minimo 4 caracteres');
         } else {
-            setSuccessFor(password)
+            passwordSuccess = true;
+            setSuccessFor(password);
         };
 
         if (password2Value === '') {
-            setErrorFor(password2, 'Metele una contraseña')
+            setErrorFor(password2, 'Tu contraseña no estaria coincidiendo');
+            password2Success = false;
         } else if (passwordValue !== password2Value) {
-            setErrorFor(password2, 'Amigo no te coinciden las contraseñas')
+            setErrorFor(password2, 'Tu contraseña no estaria coincidiendo');
+            password2Success = false;
         } else {
+            password2Success = true;
             setSuccessFor(password2);
         };
         function setErrorFor(input, message) {
